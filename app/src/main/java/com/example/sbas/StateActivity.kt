c3b49a7069753a8c8c74bbd747e5f093bfd81c96
Junseo <junso0324@naver.com>
2024-03-01 01:42:16 +0900

    Project 3/1

--- app/src/main/java/com/example/sbas/StateActivity.kt
+++ app/src/main/java/com/example/sbas/StateActivity.kt
@@ -7,6 +7,9 @@ class StateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_state)
+
+
+
         
 
     }
--- app/src/main/java/com/example/sbas/logActivity.kt
+++ app/src/main/java/com/example/sbas/logActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sbas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sbas.databinding.ActivityMainBinding
 
 class logActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+
     }
 }
